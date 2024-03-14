1de0ba47e792762b06cdde40a0b9b34693dc50e9
Sylvster Reposposa Belonio <sylvster129@gmail.com>
2024-03-14 16:07:08 +0800

    first upload

---
+++ app/src/main/java/com/networksummit/ecbusinesstools/products/ProductDetails.kt
@@ -0,0 +1,13 @@
+package com.networksummit.ecbusinesstools.products
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.networksummit.ecbusinesstools.R
+
+class ProductDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productlist)
+    }
+}
